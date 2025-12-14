f061ef82acaf7827afbd4bf090ee551c88b55640
yasin malik <ym152406@gmail.com>
2025-12-14 10:29:55 +0000

    second project

---
+++ Second project/css/index.js
@@ -0,0 +1,43 @@
+let per=10;
+if(per>=100){
+    console.log("tech error");
+    
+}
+else if(per>=90){
+    console.log("Exelent A+");
+}
+
+else if(per>=80){
+    console.log("Very Good");
+    
+}
+else if(per>=80){
+    console.log("Good");
+    
+}
+else if(per>=70){
+    console.log("fasdkjkl");
+    
+}
+else if(per>=60){
+    console.log("");
+    
+}
+let red = 13;
+switch (red) {
+    case "+":
+        console.log("add");
+        
+}
+
+const pre="+";
+switch (pre){
+    case '+':
+        console.log("poldo" ,1-2);
+        break;
+   case '-' :
+    console.log("achodan");
+        
+} 
+let t=10;
+while()
