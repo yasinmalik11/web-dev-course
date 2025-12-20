95b07a459ce47325e8b2c9f70b2d15a4b2e8560b
yasin malik <ym152406@gmail.com>
2025-12-20 10:06:50 +0000

    second project

---
+++ First project/INDES.js
@@ -0,0 +1,14 @@
+let student={
+    Name:"Yaseen",
+   Class:{
+    Class:9,
+    RollNo:34,
+    Section:"D"
+
+   },
+    Addrees:{
+        zip:34820934093,
+        aaa:"faklsld"
+    }
+}
+console.log(student.Class );
--- Second project/css/index.js
+++ Second project/css/index.js
@@ -1,43 +1,60 @@
-let per=10;
-if(per>=100){
-    console.log("tech error");
+// let per=10;
+// if(per>=100){
+//     console.log("tech error");
     
-}
-else if(per>=90){
-    console.log("Exelent A+");
-}
+// }
+// else if(per>=90){
+//     console.log("Exelent A+");
+// }
 
-else if(per>=80){
-    console.log("Very Good");
+// else if(per>=80){
+//     console.log("Very Good");
     
-}
-else if(per>=80){
-    console.log("Good");
+// }/./>/<./.
+// else if(per>=80){
+//     console.log("Good");
     
-}
-else if(per>=70){
-    console.log("fasdkjkl");
+// }
+// else if(per>=70){
+//     console.log("fasdkjkl");
     
-}
-else if(per>=60){
-    console.log("");
+// }
+// else if(per>=60){
+//     console.log("");
     
-}
-let red = 13;
-switch (red) {
-    case "+":
-        console.log("add");
+// }
+// let red = 13;
+// switch (red) {
+//     case "+":
+//         console.log("add");
         
-}
+// }
 
-const pre="+";
-switch (pre){
-    case '+':
-        console.log("poldo" ,1-2);
-        break;
-   case '-' :
-    console.log("achodan");
+// const pre="+";
+// switch (pre){
+//     case '+':
+//         console.log("poldo" ,1-2);
+//         break;
+//    case '-' :
+//     console.log("achodan");
         
-} 
-let t=10;
-while()
+// } 
+// // first projec
+// for(n=1;n<=50;n++){       
+//     if (n%5==0 ||  n%3==0) {
+//         console.log(n);
+        
+//     }
+
+// // }
+
+// // let arr=[3,4,7,8]
+log.console("loga");
+
+// let person={
+//      name:"yaseen",
+//      roll:393,
+//      class:Nine,
+// }
+// console.log(person.name);
+
